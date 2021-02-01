776b78fbd6960d4882e5401ef8174712fb067e6e
Md A Awal Hadi <awalhadi5@gmail.com>
2021-02-02 00:12:19 +0600

    true false javascript

---
+++ true-false.js
@@ -0,0 +1,29 @@
+/**
+ * falsy
+ * 0
+ * ''
+ * ""
+ * NaN
+ * undefined
+ * null
+ * false
+ */
+
+ /**
+  * truthy
+  * " "
+  * ' '
+  * '0'
+  * {}
+  * []
+  * true
+  * 'false'
+  * "true";
+  */
+
+let name = "true";
+if (name) {
+   console.log(true); 
+}else{
+    console.log(false);
+}
