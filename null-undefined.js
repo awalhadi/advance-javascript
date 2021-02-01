305f6756f29cac99a92831015999c2c38755a8fb
Md A Awal Hadi <awalhadi5@gmail.com>
2021-02-02 00:41:07 +0600

    null undefined javascript

---
+++ null-undefined.js
@@ -0,0 +1,12 @@
+let varName; //output undefined
+const testFunction = (num1, num2) => {
+    console.log(num1, num2);
+}
+
+const objName = {name:"some name", phone:"354345", country: "Bangladesh", job: "student"};
+
+const testVar = undefined;
+console.log(varName);
+console.log(testFunction(13));
+console.log(objName.gfName);
+console.log(testVar);
