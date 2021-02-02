901d53d5c8b3e0a5c086b1c3f609348e1a5148f5
Md A Awal Hadi <awalhadi5@gmail.com>
2021-02-02 15:40:19 +0600

    javascript break continue

---
+++ break-continue.js
@@ -0,0 +1,17 @@
+const nums = [1,2,3,4,5,6,7,8, -4,-3, -34];
+for (let i = 0; i < nums.length; i++) {
+    const number = nums[i];
+    if (number < -4) {
+        break;
+    }
+    console.log(number);
+}
+
+console.log("continue");
+for (let i = 0; i < nums.length; i++) {
+    const number = nums[i];
+    if (number > 0) {
+        continue;
+    }
+    console.log(number);
+}
